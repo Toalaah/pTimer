155e12091fb5867548bb8b74f0c2b83f50e0fcef
Samuel Kunst <38653851+Toalaah@users.noreply.github.com>
2019-02-28 07:55:54 +0100

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,63 +1,44 @@
 function pomodoroTimer(a) {
-    var update = document.getElementById("timer-show");
-    var initialTime = a;
     var remainingTime = a;
+    var newOne = 0
     var increment = 1;
+    var initialTime = a;
     var counter = 0;
     var ding = new Audio('bell.mp3');
-    var interval = setInterval(timeIt, 1000);
-
-    update.innerHTML = "";
-
+    var interval = setInterval(timeIt,1000);
+    
+    document.getElementById("timer-show").innerHTML="";
+    
     function convertMins(b) {
         var minutes = Math.floor(b / 60);
         var seconds = b % 60;
-        if (seconds < 10) {
+        if(seconds < 10){
             seconds = "0" + seconds;
         }
-
-        return (minutes + ":" + seconds);
+        
+        return (minutes + ":" + seconds); 
     }
-
+    
     function timerStop() {
         increment = 0;
         clearInterval(interval);
     }
 
-    document.getElementById('study25').addEventListener('click', function() {
-        timerStop();
-        return;
-    }, false);
-    document.getElementById('studyCustom').addEventListener('click', function() {
-        timerStop();
-        return;
-    }, false);
-    document.getElementById('break10').addEventListener('click', function() {
-        timerStop();
-        return;
-    }, false);
-    document.getElementById('break5').addEventListener('click', function() {
-        timerStop();
-        return;
-    }, false);
-    document.getElementById('pause').addEventListener('click', function() {
-        increment = 0;
-    }, false);
-    document.getElementById('resume').addEventListener('click', function() {
-        increment = 1;
-    }, false);
-    document.getElementById('reset').addEventListener('click', function() {
-        timerStop();
-        pomodoroTimer(initialTime);
-    }, false);
-    update.innerHTML = convertMins(remainingTime);
-
+    document.getElementById('study25').addEventListener('click', function() { timerStop(); return; }, false);
+    document.getElementById('studyCustom').addEventListener('click', function() { timerStop(); return; }, false);
+    document.getElementById('break10').addEventListener('click', function() { timerStop(); return; }, false);
+    document.getElementById('break5').addEventListener('click', function() { timerStop(); return; }, false);
+    document.getElementById('pause').addEventListener('click', function() { increment=0; }, false);
+    document.getElementById('resume').addEventListener('click', function() { increment=1; }, false);
+    document.getElementById('reset').addEventListener('click', function() { timerStop(); pomodoroTimer(initialTime); }, false);
+    document.getElementById("timer-show").innerHTML = convertMins(remainingTime);
+    
 
     function timeIt() {
-        counter = counter + increment;
-        update.innerHTML = (convertMins(remainingTime - counter));
+        counter = counter+increment;
+        document.getElementById("timer-show").innerHTML = (convertMins(remainingTime - counter));
         update_title();
-        if (counter == remainingTime) {
+        if(counter == remainingTime) {
             timerStop();
             ding.play();
         }
--- update_title.js
+++ update_title.js
@@ -1,23 +1,10 @@
 function update_title() {
-	var a = +document.getElementById('num_tasks').innerText;
 	var b = document.getElementById('timer-show').innerText;
-	if(b === "") {
-		if(a !== 0) {
-			document.title = "Dashboard | " + a + " tasks";
-		}
-		if(a === 0){
-			document.title = "Dashboard";
-		}
-	}
 	if(b !== "") {
 		document.title = b;
 	}
 	if(b === "0:00") {
-		if(a !== 0) {
-			document.title = "Dashboard | " + a + " tasks";
-		}
-		if(a === 0){
-			document.title = "Dashboard";
+		
+			document.title = "pTimer";
 		}
 	}
-}
